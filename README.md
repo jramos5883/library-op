14a76097bb7c4443966b6d37320ecbeade471041
jramos5883 <jramos5883@gmail.com>
2022-09-20 16:41:13 -0700

    Setting up for library project for Odin.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # library-op
+
+Going to build a library to improve in working with javascript.  Need to focus on object constructors and inheritence.
---
+++ script.js
@@ -0,0 +1,15 @@
+//comment
+let myLibrary = []
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = true
+    
+}
+
+function addBookToLibrary() {
+
+
+}
